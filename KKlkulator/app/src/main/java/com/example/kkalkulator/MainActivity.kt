fd865e6dfdbc4b71fdf1685cbda103d8f8f50200
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-11-19 14:21:19 +0100

    finished

--- KKlkulator/app/src/main/java/com/example/kkalkulator/MainActivity.kt
+++ KKlkulator/app/src/main/java/com/example/kkalkulator/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.NumberPicker
 import android.widget.TextView
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     lateinit var textViewInput: TextView
+    var calc: String = ""
     var num: Float = 0.0f
-    lateinit var calc: String
+    var square: Float = 0.0f
+    var squareLength: Number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit (view: View) {
+        if (textViewInput.text.toString() == "nie wolno") textViewInput.text = ""
         textViewInput.append((view as Button).text)
+    }
+
+    fun onAgain (view: View) {
+        squareLength = 0
+        square = 0.0f
+        num = 0.0f
+        textViewInput.text = ""
     }
 
     fun onClear (view: View) {
@@ -26,7 +38,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun funNum (operator: String) {
-        num = textViewInput.text.toString().toFloat()
+        if (squareLength != 0) {
+            val square2: Int = textViewInput.text.subSequence(Integer.parseInt(squareLength.toString()), textViewInput.text.toString().length).toString().toInt()
+            num = (power(square.toInt(), square2)).toFloat()
+            squareLength = 0
+            square = 0.0f
+        } else num = textViewInput.text.toString().toFloat()
+
         calc = operator
         textViewInput.text = ""
     }
@@ -36,7 +54,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSub (view: View) {
-        funNum ("subtraction")
+        if (textViewInput.text.isEmpty()) textViewInput.append("-")
+        else funNum ("subtraction")
     }
 
     fun onMul (view: View) {
@@ -47,17 +66,46 @@ class MainActivity : AppCompatActivity() {
         funNum ("division")
     }
 
+    private fun power(baseVal: Int, exponentVal: Int): Long {
+        if (exponentVal != 0) return baseVal  * power(baseVal, exponentVal - 1)
+        else return 1
+    }
+
+    fun onExp (view: View) {
+        square = textViewInput.text.toString().toFloat()
+        squareLength = square.toString().length - 1
+        textViewInput.append("^")
+    }
+
+    fun onRoot (view: View) {
+        num = textViewInput.text.toString().toFloat()
+        num = sqrt(num)
+        textViewInput.text = num.toString()
+    }
+
     fun onEqual (view: View) {
-        val someNome: Float = textViewInput.text.toString().toFloat()
-        val adding: Float = num + someNome
-        val subtraction: Float = num - someNome
-        val multiplication: Float = num * someNome
-        val division: Float = num / someNome
+        val oldTextViewInput: CharSequence = textViewInput.text
+        var someNome: Float = 0.0f
+        if (squareLength == 0) someNome = textViewInput.text.toString().toFloat()
         textViewInput.text = ""
 
-        if (calc == "adding") textViewInput.text = adding.toString()
-        else if (calc == "subtraction") textViewInput.text = subtraction.toString()
-        else if (calc == "multiplication") textViewInput.text = multiplication.toString()
-        else if (calc == "division") textViewInput.text = division.toString()
+        if (squareLength != 0){
+            textViewInput.text = oldTextViewInput
+            val square2: Int = textViewInput.text.subSequence(
+                Integer.parseInt(squareLength.toString()),
+                textViewInput.text.toString().length
+            ).toString().toInt()
+            textViewInput.text = (power(square.toInt(), square2)).toString()
+        }
+        if (calc == "adding") textViewInput.text = (num + someNome).toString()
+        else if (calc == "subtraction") textViewInput.text = (num - someNome).toString()
+        else if (calc == "multiplication") textViewInput.text = (num * someNome).toString()
+        else if (calc == "division") {
+            if (someNome == 0.0f) {
+                textViewInput.text = "nie wolno"
+                return
+            }
+            textViewInput.text = (num / someNome).toString()
+        }
     }
 }
